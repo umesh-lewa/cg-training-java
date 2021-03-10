b324a7a6bcd258469ac89c6752b24c934eef8cc9
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-10 13:11:44 +0530

    ans

---
+++ Day_2/corejavatest1/src/test1/Q.java
@@ -0,0 +1,8 @@
+package test1;
+
+public class Q { 
+    public static void main(String argv[]){ 
+            int anar[]=new int[5];
+            System.out.println(anar[0]); 
+            } 
+}
---
+++ Day_2/corejavatest1/src/test1/Scope.java
@@ -0,0 +1,14 @@
+package test1;
+
+class Base{ 
+    public void amethod(int i) {  }
+} 
+
+public class Scope extends Base{
+      public static void main(String argv[]){
+      } 
+  //Method Here
+      public void amethod(int i) throws Exception {} 
+
+
+}
