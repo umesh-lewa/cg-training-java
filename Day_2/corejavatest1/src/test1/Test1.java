0b21c88cad3dea89744f794af107cc1f11f3f5a5
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-10 14:24:24 +0530

    string comp

--- Day_2/corejavatest1/src/test1/Test1.java
+++ Day_2/corejavatest1/src/test1/Test1.java
@@ -82,9 +82,14 @@ public class Test1 {
 		System.out.println(s.indexOf('v'));
 		*/
 		
+		/*
 		String s1=new String("Hello"); 
-				String s2=new String("there"); 
-				String s3=new String();
+		String s2=new String("there"); 
+		String s3=new String();
+		s3=s1 && s2
+		*/
+		
+		System.out.println(4 | 3); 
 		
 		
 	}
