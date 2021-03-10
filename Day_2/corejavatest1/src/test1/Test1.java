e0aacd52c8c5c9be1b29a8962e218038cba1b86c
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-10 13:07:21 +0530

    ans

---
+++ Day_2/corejavatest1/src/test1/Test1.java
@@ -0,0 +1,93 @@
+package test1;
+
+public class Test1 {
+
+	public static void main(String args[]) {
+		
+		/*
+		float f=1.3; 
+		char c="a"; 
+		byte b=257; 
+		boolean b=null; 
+		int i=10; 
+		*/
+		
+		/*
+		int 2variable = 10; 
+		int variable2;
+		int whatavariable;
+		int _3_; 
+		int $anothervar; 
+		int #myvar; 
+		 */
+		
+		/*
+		int i=1; 
+        switch (i) { 
+                case 0: 
+                System.out.println("zero"); 
+        break; 
+        case 1: 
+                System.out.println("one"); 
+        case 2: 
+                System.out.println("two"); 
+        default: 
+                System.out.println("default"); 
+        }
+        */
+		
+		/*
+		int i=9; 
+		switch (i) { 
+		        default: 
+		        System.out.println("default"); 
+		                case 0: 
+		        System.out.println("zero"); 
+		                break; 
+		        case 1: 
+		                System.out.println("one"); 
+		        case 2: 
+		        System.out.println("two"); 
+		}
+		*/
+		
+		/*
+		int i=1; 
+		int j=2; 
+		if(i==1 &| j==2) 
+		        System.out.println("OK");
+        */
+		
+		/*
+		System.out.println(Math.ceil(-4.7)); 
+		*/
+		
+		/*
+		Integer ten=new Integer(10); 
+		Long nine=new Long (9);
+		System.out.println(ten + nine); 
+		int i=1;
+		System.out.println(i + ten);
+		*/
+		
+		/*
+		String s=new String("Bicycle"); 
+		int iBegin=1; 
+		char iEnd=3; 
+		System.out.println(s.substring(iBegin,iEnd));
+		*/
+		
+		/*
+		String s = "Java";
+		System.out.println(s.indexOf('v'));
+		*/
+		
+		String s1=new String("Hello"); 
+				String s2=new String("there"); 
+				String s3=new String();
+		
+		
+	}
+}
+
+
