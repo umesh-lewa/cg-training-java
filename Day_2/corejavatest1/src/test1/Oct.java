b0e3f7a3ff1b630c7f97bdbcb00d8e98ba4a6885
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-10 15:18:36 +0530

    oct

---
+++ Day_2/corejavatest1/src/test1/Oct.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Oct{
+public static void main(String argv[]){
+        Oct o = new Oct();
+        o.amethod();
+        }
+        public void amethod(){
+        int oi= 012;
+        System.out.println(oi);
+        }
+}
