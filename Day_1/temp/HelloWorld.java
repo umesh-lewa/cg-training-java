4e92310f774bdba5a9192846c47d0570c34c6690
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-10 17:33:00 +0530

    d1

---
+++ Day_1/temp/HelloWorld.java
@@ -0,0 +1,8 @@
+package pack1;
+
+import pack3.Hello3;
+
+public class HelloWorld{
+
+	Hello3 he3;
+}
---
+++ Day_2/corejavatest1/src/test1/Rand.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class Rand{
+public static void main(String argv[]){
+        int iRand;
+        iRand = Math.random();
+        System.out.println(iRand);
+        }
+}
