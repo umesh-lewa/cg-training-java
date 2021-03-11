8f588d3b26d22182f512a1ed875c631522358b78
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 14:20:56 +0530

    q9

---
+++ Day_3/BasicsLabBook/src/labbook1/Q9.java
@@ -0,0 +1,15 @@
+package labbook1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Q9 {
+
+  
+    public static void main(String[] args) {
+    	
+        int [] arr = {1,2,3,4,5};
+        Collections.reverse(Arrays.asList(arr)); 
+        System.out.println(Arrays.asList(arr)); 
+    } 
+}
