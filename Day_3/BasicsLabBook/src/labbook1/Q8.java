fbd261d52d585f046b176333f4c4f2dfacf94728
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 14:21:08 +0530

    q8

---
+++ Day_3/BasicsLabBook/src/labbook1/Q8.java
@@ -0,0 +1,17 @@
+package labbook1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q8 {
+
+    public static void main(String[] args)
+    {
+    	Integer[] arr = {1,34,56,13,76,45,21};
+
+    	System.out.println(Arrays.asList(arr).indexOf(13));
+  
+    }
+    
+}
