184f070bd97dc8873f2f2d199016e36b980df3ec
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 10:10:15 +0530

    ref

---
+++ Day_2/corejavatest1/src/test1/Ref.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class Ref{
+public static void main(String argv[]){
+          Ref r = new Ref();
+          r.amethod(r);
+          }
+          public void amethod(Ref r){
+          int i=99;
+          multi(r);
+          System.out.println(i);
+          }
+          public void multi(Ref r){
+          r.i = r.i*2;
+          }
+}
