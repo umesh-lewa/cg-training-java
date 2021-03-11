df0a80892714dce31dea63a29468a15511fcf67b
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 12:24:32 +0530

    q2

---
+++ Day_3/BasicsLabBook/src/labbook/Q2.java
@@ -0,0 +1,20 @@
+package labbook;
+
+public class Q2 {
+
+	static boolean bool;
+	static double doub;
+	static float fl;
+	static int in;
+	static long lon;
+	
+	public static void main(String args[]) {
+				
+		System.out.println(bool);
+		System.out.println(doub);
+		System.out.println(fl);
+		System.out.println(in);
+		System.out.println(lon);
+		
+	}
+}
