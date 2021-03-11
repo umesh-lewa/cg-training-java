a2dc38d8780068e54c7a020d466b20d186e9cccb
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 10:10:30 +0530

    lin

---
+++ Day_2/corejavatest1/src/test1/Lin.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class Lin{
+public static void main(String argv[]){
+        Lin l = new Lin();
+        l.amethod(); 
+        }
+        public void amethod(){
+        int ia[] = new int[4];
+                        //Start For loop        
+                        {
+           ia[i]=i;
+           System.out.println(ia[i]);
+           }
+        }
+}
