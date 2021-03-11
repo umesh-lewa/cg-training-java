d04f25a4a470e3e197e503ad8133dfb39360c67b
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 13:47:33 +0530

    q6

---
+++ Day_3/BasicsLabBook/src/labbook/Q6.java
@@ -0,0 +1,15 @@
+package labbook;
+
+import java.util.Arrays;
+
+public class Q6 {
+	
+	public static void main(String args[]) {
+		
+		int [] arr = new int [] {85,9,66,76};  
+		Arrays.sort(arr);   
+		System.out.println(Arrays.toString(arr));  
+		
+	}
+
+}
