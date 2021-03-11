d3150a975e965915420aace116fbd682186ee837
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 12:42:50 +0530

    q5

---
+++ Day_3/BasicsLabBook/src/labbook/Q5.java
@@ -0,0 +1,25 @@
+package labbook;
+
+public class Q5 {
+
+	public static void main(String args[]) {
+		
+		Q5 obj = new Q5();
+		int[] numbers = new int[]{1,2,3,4,5,6,7};
+		System.out.println(obj.calcAverage(numbers));
+	}
+	
+	public float calcAverage(int[] numbers) {
+		
+	       int sum = 0;
+	       
+	       for(int i=0; i < numbers.length ; i++) {
+	        sum = sum + numbers[i];
+	       }
+	       
+	       float average = sum / numbers.length;
+	        
+	       return average;
+	}
+	
+}
