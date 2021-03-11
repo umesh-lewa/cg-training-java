b1af74102e67d2309282df1abbfee7ffa013c875
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-11 17:14:37 +0530

    q22

---
+++ Day_3/BasicsLabBook/src/labbook1/Q22.java
@@ -0,0 +1,33 @@
+package labbook1;
+
+public class Q22 {
+
+	public static void main(String args[]) {
+		
+		SubClass obj = new SubClass();
+		
+		
+	}
+	
+	
+}
+
+class SubClass {
+	
+	private int privMember  = 10;
+	protected int protectedMember = 10;
+	public int publicMember = 10;
+	int defaultMember = 10;
+	
+	public void pubMethod() {
+		
+	}
+	
+	protected void protecMethod() {
+		
+	}
+	
+	private void privMethod() {
+		
+	}
+}
