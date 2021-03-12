841b9fabe6fe4f4ed2d2ec492a72e3fdc29edf24
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 11:29:41 +0530

    q13

--- Day_3/BasicsLabBook/src/labbook1/Q13.java
+++ Day_3/BasicsLabBook/src/labbook1/Q13.java
@@ -1,5 +1,39 @@
 package labbook1;
 
+import java.util.Scanner;
+
 public class Q13 {
 
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int studentsCount = 5;
+		int studentsMarks[][] = new int[studentsCount][3];
+		
+		for(int i = 0; i < studentsCount; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.println("Enter Mark of Student "+ (i + 1) + " on subject  "+ (j + 1));
+				studentsMarks[i][j] = scan.nextInt();
+			}
+		}
+		
+		for(int i = 0; i < studentsCount; i++) {
+			double avg = getAvg(studentsMarks[i]);
+			System.out.println("Student " + (i+1) + " Total : " + (int)(avg * 3) + "  Average : " + avg);
+		}
+		
+	}
+	
+	static double getAvg(int[] arr) {
+		
+		int sum = 0;
+		
+		for(int i : arr) {
+			sum += i;
+		}
+		
+		return sum / arr.length;
+	}
+	
 }
