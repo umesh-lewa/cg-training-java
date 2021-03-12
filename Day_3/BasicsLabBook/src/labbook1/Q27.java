d13bd6578dbee6d5f66b19ab0570e13463177437
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 14:29:00 +0530

    q27

---
+++ Day_3/BasicsLabBook/src/labbook1/Q27.java
@@ -0,0 +1,17 @@
+package labbook1;
+
+public class Q27 {
+
+	public static void main(String args[]) {
+		
+		int baseValue = 14000;
+		
+		baseValue = baseValue + ( (40 / 100 ) * baseValue);
+		
+		baseValue = baseValue - 1500;
+		
+		baseValue = baseValue + ( (12 / 100 ) * baseValue);
+		
+	}
+	
+}
