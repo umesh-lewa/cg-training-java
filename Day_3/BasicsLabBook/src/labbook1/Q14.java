3f1916e07e403e4f80710dd07fc45a28c4f3dbff
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 11:37:32 +0530

    q14

--- Day_3/BasicsLabBook/src/labbook1/Q14.java
+++ Day_3/BasicsLabBook/src/labbook1/Q14.java
@@ -1,6 +1,18 @@
 package labbook1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Q14 {
 
+	public static void main(String args[]) {
+		
+		int arr[] = {1,2,3};
+		
+		List arrList = Arrays.asList(arr);
+		arrList.add(4);
+		
+	}
 	
 }
