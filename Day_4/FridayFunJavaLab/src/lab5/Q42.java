d283471349da6ccdc43b8f51b2aa25efa4afd395
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 15:42:35 +0530

    q42

---
+++ Day_4/FridayFunJavaLab/src/lab5/Q42.java
@@ -0,0 +1,16 @@
+package lab5;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Q42 {
+	
+    public static void main(String[] args) 
+    { 
+        Integer [] arr = {10, 20, 30, 40, 50};
+        Collections.reverse(Arrays.asList(arr)); 
+        System.out.println(Arrays.asList(arr)); 
+
+    } 
+
+}
