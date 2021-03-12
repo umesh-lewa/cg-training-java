36ea0b81f8d6b6e66f37f1bb232117fc10670314
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 10:44:55 +0530

    q15

---
+++ Day_3/BasicsLabBook/src/labbook2/Q15.java
@@ -0,0 +1,59 @@
+package labbook2;
+
+public class Q15 {
+
+	class Door{
+		
+		int area = 1;
+		int costPerSqArea = 4;
+		
+		public int calcCost() {
+			
+			return this.area * this.costPerSqArea;
+		}
+	}
+	
+	class Window{
+		
+		int area = 1;
+		int costPerSqArea = 6;
+		
+		public int calcCost() {
+			
+			return this.area * this.costPerSqArea;
+		}
+	}
+	
+	class House{
+		
+		int noOfDoors = 0;
+		int noOfWindows = 0;
+		Door door = new Door();
+		Window window = new Window();
+
+		
+		House(int noOfDoors, int noOfWindows){
+			this.noOfDoors = noOfDoors;
+			this.noOfWindows = noOfWindows;
+		}
+		
+		public int calcHouseCost() {
+			
+			//int totalDoorCost = 0;
+			//int totalWindowCost = 0;
+			
+			int costPerDoor = door.calcCost();
+			int costPerWindow = window.calcCost();
+			
+			
+			return (this.noOfDoors * costPerDoor) + (this.noOfWindows * costPerWindow);
+		}
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		House houseOj = new House(4,15);
+		int totalCost = houseOj.calcHouseCost();
+	}
+}
