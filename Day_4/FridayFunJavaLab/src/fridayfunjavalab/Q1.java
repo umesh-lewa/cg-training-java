9412e8659e84f1956ab7ff4217b0c3be19e9ba8d
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 14:10:11 +0530

    q1

---
+++ Day_4/FridayFunJavaLab/src/fridayfunjavalab/Q1.java
@@ -0,0 +1,18 @@
+package fridayfunjavalab;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+
+		 int a, b;
+		 a = 15;
+		 b = 27;
+		 System.out.println("Before swapping : a, b = "+a+", "+ + b);
+		 a = a + b;
+		 b = a - b;
+		 a = a - b;
+		 System.out.println("After swapping : a, b = "+a+", "+ + b);
+		 
+		 } 
+	
+}
