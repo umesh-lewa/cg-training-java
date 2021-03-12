93980820bb919977bf3b673c5a3dc0099325022b
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 09:53:52 +0530

    q9

---
+++ Day_3/BasicsLabBook/src/labbook2/Q9.java
@@ -0,0 +1,27 @@
+package labbook2;
+
+class A{
+	
+	public int pubMem;
+	private int priMem;
+	protected int proMem;
+	int defaultMem;
+	
+}
+public class Q9 extends A{
+	
+	public static void main(String args[]) {
+		
+		
+	}
+	
+	public void getBaseClassMem() {
+		
+		System.out.println(pubMem);
+		System.out.println(priMem);
+		System.out.println(proMem);
+		System.out.println(defaultMem);
+	}
+	
+	
+}
