251fcded887c99eb353b44d2ac030ed3e945b72e
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 09:49:27 +0530

    q13

---
+++ Day_3/BasicsLabBook/src/labbook2/Q13.java
@@ -0,0 +1,14 @@
+package labbook2;
+
+public class Q13 {
+
+	abstract class AbstractTest{
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		AbstractTest obj = new AbstractTest();
+		
+	}
+}
