e4489d00ae1924230f100e35de8774b5292f16d9
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-12 11:12:00 +0530

    q3

--- Day_3/BasicsLabBook/src/labbook1/Q3.java
+++ Day_3/BasicsLabBook/src/labbook1/Q3.java
@@ -2,4 +2,32 @@ package labbook1;
 
 public class Q3 {
 
+	public static void main(String[] args) {
+		
+		int i = 50;
+		byte b = 10;
+		short s = 5;
+		char a = 'a';
+		String str = "Str";
+		float f = 5.5f;
+		double d = 33.33;
+		
+		long doubleToLong = (long) d;
+		int longToInt = (int) doubleToLong;
+		long intToShort = (short) longToInt;
+		float shortToByte = (byte) intToShort;
+		
+		short byteToShort = s;
+		int shortToInt = byteToShort;
+		long intToLong = shortToInt;
+		float longToFloat = intToLong;
+		
+	
+		char ch = 'z';
+		int num = 999;
+		char chara = (char)num;
+		int numch = ((int)ch);
+
+	}
+	
 }
