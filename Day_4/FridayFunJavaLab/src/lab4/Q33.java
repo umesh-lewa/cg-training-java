c1bc0faa3b3a3cbea00b21a8f9976f2a0a2f191d
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-15 09:10:18 +0530

    q33

---
+++ Day_4/FridayFunJavaLab/src/lab4/Q33.java
@@ -0,0 +1,19 @@
+package lab4;
+
+public class Q33 {
+	
+	public static void main(String args[]) {
+		
+		int lim = 50, i;  
+
+		i=2;   
+ 
+		while(i<=lim) {  
+			
+		System.out.print(i +" ");   
+		i=i+2;  
+		}     
+		
+	}
+
+}
