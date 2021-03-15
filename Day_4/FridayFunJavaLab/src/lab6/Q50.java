d6f11d37d3277a3a850f5717ac33d42d16d89eaf
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-15 10:14:42 +0530

    q50

---
+++ Day_4/FridayFunJavaLab/src/lab6/Q50.java
@@ -0,0 +1,21 @@
+package lab6;
+
+class GrandParent{
+	
+}
+
+class Parent extends GrandParent{
+	
+}
+
+public class Q50 extends Parent {
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
+
+class Child extends Q50{
+	
+}
