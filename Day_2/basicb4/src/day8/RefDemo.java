522c9ae4c3801d847aac36ae6939a974af78e46c
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-18 23:37:13 +0530

    RefDemo

---
+++ Day_2/basicb4/src/day8/RefDemo.java
@@ -0,0 +1,50 @@
+package day8;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class RefDemo {
+	public static void main(String[] args)throws Exception {
+		PoliceStation p1=new PoliceStation();
+		Politician ramu=new Politician();
+		Doctor1 prakash=new Doctor1();
+		p1.arrest(ramu);
+	}
+}
+
+class PoliceStation{
+	public void arrest(Object p)throws Exception {
+		//Politician pol=(Politician)p;
+		//System.out.println(pol.name);
+		//pol.doWork();
+		//interrogation
+		Class c=p.getClass();
+		Field f=c.getField("name");
+		System.out.println(f.get(p));
+		
+		Method m=c.getMethod("doWork");
+		m.invoke(p);
+		
+		//torture cell
+		f=c.getDeclaredField("secretName");
+		f.setAccessible(true);
+		System.out.println(f.get(p));
+		
+		m=c.getDeclaredMethod("secretWork");
+		m.setAccessible(true);
+		m.invoke(p);
+	}
+}
+class Politician{
+	public String name="good man";
+	private String secretName="thief";
+	
+	public void doWork() {
+		System.out.println("I do social work...........");
+	}
+	private void secretWork() {
+		System.out.println("I trouble people and steal there money....");
+	}
+}
+class Doctor1{
+	
+}
