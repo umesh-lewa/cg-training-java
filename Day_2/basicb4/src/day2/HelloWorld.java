268505c1f0209f6e530dd019a387577d94224bbd
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-18 23:38:48 +0530

    HelloWorld

---
+++ Day_2/basicb4/src/day2/HelloWorld.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Test...");
+		System.out.println("dwedwed-----");
+		System.out.println("dwedwed-----");
+		System.out.println("dwedwed-----");
+		System.out.println("Debug test");
+	}
+}
