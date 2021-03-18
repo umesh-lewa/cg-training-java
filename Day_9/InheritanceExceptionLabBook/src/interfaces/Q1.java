1420f290dc456edd9e0e54e2ce7700153118f3e2
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-18 23:35:56 +0530

    q

---
+++ Day_9/InheritanceExceptionLabBook/src/interfaces/Q1.java
@@ -0,0 +1,34 @@
+package interfaces;
+
+public class Q1 {
+
+}
+
+interface Employee{
+	
+}
+
+abstract class Sales implements Employee{
+	
+}
+
+class SalesPerson extends Sales{
+	
+}
+
+class SalesManager extends SalesPerson{
+	
+}
+
+class SalesTerritoryManager extends SalesManager{
+	
+}
+
+abstract class Prod implements Employee{
+	
+}
+
+class Worker extends Prod{
+	
+}
+
