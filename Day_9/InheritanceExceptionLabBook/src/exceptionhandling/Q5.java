e7349e9b1e26c01c0feb31f67892c5cf8d08b2ed
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-19 14:43:05 +0530

    q5

---
+++ Day_9/InheritanceExceptionLabBook/src/exceptionhandling/Q5.java
@@ -0,0 +1,13 @@
+package exceptionhandling;
+
+public class Q5 {
+
+	public static void main(String args[]) {
+		
+		
+	}
+	
+	public void throwingMethod() throws CustomException{
+		throw new CustomException();
+	}
+}
