e06f13f9416d5a54d16243728dac48d67ff5ba9d
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-19 10:17:26 +0530

    q2

---
+++ Day_9/InheritanceExceptionLabBook/src/exceptionhandling/Q2.java
@@ -0,0 +1,30 @@
+package exceptionhandling;
+
+public class Q2 {
+
+	public static void main(String p[])
+	{
+	sample s=new
+	sample();
+	s.mth1();
+	}
+}
+
+class sample{ 
+	public void mth1(){
+		mth2(); 
+		System.out.println("caller");
+	}
+public void mth2(){
+	
+try{
+throw new RuntimeException();
+}catch(Exception e){ System.out.println("catch-mth2");}
+/*
+B-30 Twin Courtz Apartments, Anna High Road, Perungudi, Chennai –600096
+GSTIN - 33AJDPM3302R1Z6 – Cell Phone: 9514234319 / 7448576014
+*/
+finally{System.out.println("finally-mth2");}
+}
+
+}
