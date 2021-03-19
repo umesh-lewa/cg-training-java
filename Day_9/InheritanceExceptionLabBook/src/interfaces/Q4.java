a1dbb5450921fb9a8c50532b2c932876eff015df
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-19 17:27:49 +0530

    q4

--- Day_9/InheritanceExceptionLabBook/src/interfaces/Q4.java
+++ Day_9/InheritanceExceptionLabBook/src/interfaces/Q4.java
@@ -2,4 +2,43 @@ package interfaces;
 
 public class Q4 {
 
+	
 }
+
+interface Inter1{
+	
+	public void met1();
+	
+	public void met2();	
+}
+
+interface Inter2{
+	
+	public void met3();
+	
+	public void met4();
+	
+}
+
+interface Inter3{
+	
+    public void met5();
+	
+	public void met6(); 
+	
+}
+
+interface Inter4 extends Inter1, Inter2, Inter3{
+	
+	public void newMet();
+}
+
+class Cl1 implements Inter4{
+	
+	@Override
+	public void newMet(){
+		
+	}
+}
+
+class Cl2
