24f7737cbff7ab0b3da7b369b89942fcd8d1286d
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-19 17:25:42 +0530

    frmt

--- Day_2/basicb4/src/day8/DecoratorDemo.java
+++ Day_2/basicb4/src/day8/DecoratorDemo.java
@@ -1,31 +1,41 @@
 package day8;
 
 public class DecoratorDemo {
+	
 	public static void main(String[] args) {
+		
 		Food thali=new Rice(new Daal(new Chicken(new Rice())));
 		System.out.println("Cost of Rice..+Daal..+chicken+rice..:"+thali.cost());
+		
 	}
 }
 //static composition - static composition is achieved through generalization - part whole hierarchy
 abstract class Food{
 	public abstract int cost();
 }
+
 abstract class VegFood extends Food{
 	
 }
+
 abstract class NonVegFood extends Food{
 	
 }
+
 //To do static composition we need to use the constructor
 class Rice extends VegFood{
+	
 	public Rice() {
 		// TODO Auto-generated constructor stub
 	}
+	
 	private Food food;
+	
 	public Rice(Food food) {
 		this.food=food;
 	}
-@Override
+	
+	@Override
 	public int cost() {
 		// TODO Auto-generated method stub
 	if(food!=null) {
@@ -36,15 +46,20 @@ class Rice extends VegFood{
 		}
 	}
 }
+
 class Daal extends VegFood{
+	
 	public Daal() {
 		// TODO Auto-generated constructor stub
 	}
+	
 	private Food food;
+	
 	public Daal(Food food) {
 		this.food=food;
 	}
-@Override
+	
+	@Override
 	public int cost() {
 		// TODO Auto-generated method stub
 	if(food!=null) {
@@ -56,14 +71,18 @@ class Daal extends VegFood{
 	}
 }
 class Chicken extends NonVegFood{
+	
 	public Chicken() {
 		// TODO Auto-generated constructor stub
 	}
+	
 	private Food food;
+	
 	public Chicken(Food food) {
 		this.food=food;
 	}
-@Override
+	
+	@Override
 	public int cost() {
 		// TODO Auto-generated method stub
 	if(food!=null) {
@@ -73,4 +92,5 @@ class Chicken extends NonVegFood{
 			return 20;
 		}
 	}
+	
 }
