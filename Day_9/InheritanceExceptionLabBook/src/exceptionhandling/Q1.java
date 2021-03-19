9585453d3917a0d3a3df49868c80430a6654eee8
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-19 10:11:41 +0530

    q1

---
+++ Day_9/InheritanceExceptionLabBook/src/exceptionhandling/Q1.java
@@ -0,0 +1,18 @@
+package exceptionhandling;
+
+import java.io.IOException;
+
+public class Q1 {
+
+	public static void main(String args[]) {
+		
+	}
+}
+
+class Thrower{
+	
+	public void throwingMethod() {
+		
+		throw new IOException();
+	}
+}
