3832d490d2c7dfaa7f7db42d6513a37ed1da47bf
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-22 17:44:38 +0530

    CallableDemo

---
+++ Day_2/basicb4/src/day11/CallableDemo.java
@@ -0,0 +1,35 @@
+package day11;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableDemo {
+	
+	public static void main(String[] args)throws Exception {
+		
+		ExecutorService es=Executors.newFixedThreadPool(1);
+		
+		Future f=es.submit(new MyCallable());
+		
+		String s=(String)f.get();
+		
+		System.out.println(s);
+		
+		System.out.println("main thread....");
+		es.shutdown();
+	}
+	
+}
+
+class MyCallable implements Callable{
+	
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		try {Thread.sleep(2000);}catch(Exception e) {}
+		return "hello world";
+	}
+	
+}
