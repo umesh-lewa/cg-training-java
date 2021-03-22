c7e00d834eb5741fe3dbcaca5501d51e7271c55a
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-22 12:45:17 +0530

    TwoThreadDemo

---
+++ Day_2/basicb4/src/day11/TwoThreadDemo.java
@@ -0,0 +1,25 @@
+package day11;
+
+public class TwoThreadDemo {
+	
+	public static void main(String args[]) {
+
+		System.out.println("first line.....");
+		//new Thread(()->{met();}).start();
+		met();
+		System.out.println("third line...");
+		
+	}
+	
+	public static void met(){
+		
+		try{
+			Thread.sleep(5000);
+		}catch(Exception e) {
+			
+		}
+		System.out.println("second line...");
+		
+	}
+
+}
