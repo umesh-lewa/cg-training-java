8c366d57bdd7f9c99d3cbe39f98a7029a3fbc663
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-23 10:22:23 +0530

    fr

--- Day_2/basicb4/src/day11/SingleObjectTwoThreadsOneTask.java
+++ Day_2/basicb4/src/day11/SingleObjectTwoThreadsOneTask.java
@@ -8,21 +8,29 @@ public class SingleObjectTwoThreadsOneTask {
 	public static void main(String[] args) {
 		
 		Gun bofors=new Gun();
+		
 		ExecutorService es=Executors.newFixedThreadPool(2);
 		
 		es.execute(()->{
+			
 			Thread.currentThread().setName("filler");
+			
 			for(int i=0;i<5;i++) {
 				bofors.fill();
 			}
+			
 		});
 		
 		es.execute(()->{
+			
 			Thread.currentThread().setName("shooter");
+			
 			for(int i=0;i<5;i++) {
 				bofors.shoot();
-			}			
-		});		
+			}
+			
+		});
+		
 		es.shutdown();		
 	}
 	
@@ -35,17 +43,20 @@ class Gun{
 	synchronized public void fill() {
 		
 		if(flag) {
+			
 			try {
 				//Thread.sleep(5000);
 				wait();
 			}catch(Exception e) {
 				
 			}
+			
 		}
 		
 		System.out.println("fill the gun..............");
 		
 		flag=true;
+		
 		notify();
 		
 	}
@@ -53,17 +64,20 @@ class Gun{
 	synchronized public void shoot() {
 		
 		if(!flag) {
+			
 			try {
 				//Thread.sleep(5000);
 				wait();
 			}catch(Exception e) {
 				
 			}
+			
 		}
 		
 		System.out.println("shoot the gun.....");
 		
 		flag=false;
+		
 		notify();//it will notify the  waiting filler thread inside the monitor
 		
 	}
