32c6ce40956327050cbeaedc151f78a4eb13ac70
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-24 17:02:02 +0530

    q3

---
+++ Day_13/CollectionsLabBook/src/collectionLabBook/Q3.java
@@ -0,0 +1,16 @@
+package collectionLabBook;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Q3 {
+	
+	public static void before() { Set set = new TreeSet(); set.add("2");
+	set.add(3);
+	set.add("1");
+	Iterator it = set.iterator(); while (it.hasNext())
+	 
+	System.out.print(it.next() + " "); }
+
+}
