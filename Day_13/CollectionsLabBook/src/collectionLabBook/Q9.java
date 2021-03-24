f6be10892825c997a66d660a45b0e60956ef7ecf
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-24 17:02:52 +0530

    q9

---
+++ Day_13/CollectionsLabBook/src/collectionLabBook/Q9.java
@@ -0,0 +1,20 @@
+package collectionLabBook;
+
+import java.util.*;
+
+public class Q9 {
+	
+public static void main(String[] args) { 
+	Object o = new Object();
+// insert code here
+	
+	
+	Set s = new HashSet();
+	//TreeSet s = new TreeSet();
+	//LinkedHashSet s = new LinkedHashSet();
+s.add("o");
+s.add(o);
+}
+}
+
+// A B C D F
