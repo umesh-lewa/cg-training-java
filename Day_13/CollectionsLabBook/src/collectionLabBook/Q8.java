5c0b6f9a12b600d2dbf61a18a4fe822af318f4be
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-24 17:02:41 +0530

    q8

---
+++ Day_13/CollectionsLabBook/src/collectionLabBook/Q8.java
@@ -0,0 +1,20 @@
+package collectionLabBook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q8 {
+	
+	public static void main(String args[]) {
+		
+		List<Integer> input = null; List<Integer> output = null;
+		
+		output = process(input);
+	}
+
+	public static <E extends Number> List<E> process(List<E> nums){
+		
+		return new ArrayList<E>();
+	}
+		
+}
