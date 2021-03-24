2746c81c739a45d254f9cdb54dd31762a4c1cf49
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-24 17:02:34 +0530

    q7

---
+++ Day_13/CollectionsLabBook/src/collectionLabBook/Q7.java
@@ -0,0 +1,53 @@
+package collectionLabBook;
+
+public class Q7 {
+
+}
+
+interface Hungry<E> { 
+	
+	void munch(E x); 
+	
+	}
+
+interface Carnivore<E extends Animal> extends Hungry<E> {
+	
+}
+
+interface Herbivore<E extends Plant> extends Hungry<E> {
+	
+}
+
+abstract class Plant {
+	
+}
+
+class Grass extends Plant {
+	
+}
+
+abstract class Animal {
+	
+}
+
+class Sheep extends Animal implements Herbivore<Plant> {
+	
+public void munch(Sheep x) {
+	
+}
+
+@Override
+public void munch(Plant x) {
+	// TODO Auto-generated method stub
+	
+}
+
+}
+
+class Wolf extends Animal implements Carnivore<Sheep> {
+	
+public void munch(Sheep x) {
+	
+}
+
+}
