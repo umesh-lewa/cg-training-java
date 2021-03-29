92bef19cae842261b3fb016df4e6751bc3ff4836
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-29 10:26:12 +0530

    filedemo

--- Day_15/IOLabBook/src/iolabbook/filedemo.java
+++ Day_15/IOLabBook/src/iolabbook/filedemo.java
@@ -1,5 +1,22 @@
 package iolabbook;
 
-public class filedemo {
+import java.io.File;
 
+class filedemo {
+	
+static void p(String s) {
+	
+System.out.println(s);  }
+
+public static void main(String args[]){
+	
+File f1=new File(“abc.txt”);
+
+p(“file name :”+f1.getName());
+p(“path :”+f1.getPath());
+p(f1.exists() ? “exists”:”does not exist”);
+p(f1.canWrite() ? “is writable”:”is not writable”);
+p(f1.isDirectory() ?” is a dir”:”is not a directory”);
 }
+}
+
