4dbafb0285cebf9601e6485efcfbafabcb4b7fb5
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-03-30 10:24:15 +0530

    fr

--- Day_2/basicb4/src/day13/QuizApp.java
+++ Day_2/basicb4/src/day13/QuizApp.java
@@ -8,10 +8,9 @@ import java.util.TreeSet;
 
 public class QuizApp {
 	
-public static void main(String args[]) {
+	public static void main(String args[]) {
 		
-		
-		
+				
 		Scanner scanner = new Scanner(System.in);
 	    System.out.println("Enter No of Questions :");
 
