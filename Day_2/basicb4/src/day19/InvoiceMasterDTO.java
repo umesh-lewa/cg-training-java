52e0e8e34af5e5006acc0c4f124e9d3d0d4ac39c
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-01 14:16:27 +0530

    InvoiceMasterDTO

---
+++ Day_2/basicb4/src/day19/InvoiceMasterDTO.java
@@ -0,0 +1,64 @@
+package day19;
+
+import java.io.Serializable;
+
+public class InvoiceMasterDTO implements Serializable,Cloneable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5862830728212664563L;
+	
+	private InvoiceMasterDTO invoiceMasterDTO;
+	
+	private int invid;
+	private String invdate;
+	private String customerid;
+	
+	public InvoiceMasterDTO getInvoiceMasterDTO() {
+		
+		if(invoiceMasterDTO==null) {
+			invoiceMasterDTO=new InvoiceMasterDTO();
+		}
+		
+		return invoiceMasterDTO.getCloneInvoiceMasterDTO();
+	}
+	
+	private InvoiceMasterDTO getCloneInvoiceMasterDTO() {
+		
+		try {
+			return (InvoiceMasterDTO)super.clone();
+		} catch (CloneNotSupportedException e) {
+			
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "InvoiceMasterDTO [invid=" + invid + ", invdate=" + invdate + ", customerid=" + customerid + "]";
+	}
+	
+	public int getInvid() {
+		return invid;
+	}
+	public void setInvid(int invid) {
+		this.invid = invid;
+	}
+	public String getInvdate() {
+		return invdate;
+	}
+	public void setInvdate(String invdate) {
+		this.invdate = invdate;
+	}
+	public String getCustomerid() {
+		return customerid;
+	}
+	public void setCustomerid(String customerid) {
+		this.customerid = customerid;
+	}
+	
+	
+
+}
