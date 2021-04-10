f64e33c705162b42ebd8d3d4c86bcee904cf2b66
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-10 16:21:30 +0530

    QueryClient

---
+++ Day_2/basicb4/src/day21/QueryClient.java
@@ -0,0 +1,15 @@
+package day21;
+
+import org.hibernate.Session;
+import org.hibernate.Citeris;
+
+public class QueryClient {
+	
+	public static void main(String args[]) {
+		
+		Session session = HibernateUtility.getSession();
+		
+		Criteria crit = session.
+	}
+
+}
