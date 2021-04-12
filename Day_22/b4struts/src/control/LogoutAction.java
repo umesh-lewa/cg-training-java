87f70e1fe32a1973708018cfead9b85eb75dec88
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-12 17:06:37 +0530

    logout

--- Day_22/b4struts/src/control/LogoutAction.java
+++ Day_22/b4struts/src/control/LogoutAction.java
@@ -4,16 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import daopack.UserDAOImpl;
 import servicepack.LoginServiceImpl;
 
 public class LogoutAction extends Action {
  @Override
 public String execute(HttpServletRequest request, HttpServletResponse response) {
+	 
 	HttpSession session=request.getSession();
 	String uname=session.getAttribute("uname").toString();
 	
 	LoginServiceImpl lservice=LoginServiceImpl.getLoginService();
-	
+	lservice.setUserDAO(UserDAOImpl.getUserDaoImpl());
+	lservice.setServCtx(request.getServletContext());
 	lservice.updateFlag(uname, 0);
 	
 	return "logout.success";
