c64828c83f5a5e3316474efa0ca9e523736733b9
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-12 17:06:50 +0530

    lang dictionaries

---
+++ Day_22/b4struts/src/control/Dictionary_ta.java
@@ -0,0 +1,17 @@
+package control;
+
+import java.util.ListResourceBundle;
+
+public class Dictionary_ta extends ListResourceBundle{
+	Object obj[][]= {
+			{"username","பயனர்பெயர்"},
+			{"password","கடவுச்சொல்"}
+	};
+@Override
+protected Object[][] getContents() {
+	// TODO Auto-generated method stub
+	return obj;
+}
+	
+}
+
--- Day_22/b4struts/src/control/LangAction.java
+++ Day_22/b4struts/src/control/LangAction.java
@@ -11,10 +11,12 @@ public class LangAction extends Action{
 	
 @Override
 public String execute(HttpServletRequest request, HttpServletResponse response) {
+	
 		String language=request.getParameter("language");
 		ResourceBundle rb=ResourceBundle.getBundle("control.Dictionary",new Locale(language));
 		HttpSession session=request.getSession();
 		session.setAttribute("rb", rb);
+		
 		return "lang.success";
 	}
 }
