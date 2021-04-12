f076e014e5d0c4c64bc0c049d28aee438373756d
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-12 12:34:26 +0530

    MySessionFilter

---
+++ Day_22/b4struts/src/control/MySessionFilter.java
@@ -0,0 +1,66 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class MySessionFilter
+ */
+public class MySessionFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MySessionFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		HttpServletRequest hrequest = (HttpServletRequest)request;
+		HttpServletResponse hresponse = (HttpServletResponse)response;
+		
+		HttpSession session = hrequest.getSession();
+		
+		if(session.isNew()) {
+			String formid = request.getParameter("formid");
+			if(formid.equals("lang")) {
+				chain.doFilter(request, response);
+			}else {
+				hresponse.sendRedirect("sessionexpiry.jsp");
+			}
+		}else {
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
