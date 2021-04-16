7335693c02c9ba730ae6519a20c6521bb18ac3fb
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-16 11:56:46 +0530

    coda4ws

---
+++ Day_27/codaws/src/codaws/HelloService.java
@@ -0,0 +1,12 @@
+package codaws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface HelloService {
+	
+	@WebMethod
+	public String sayHello(String name);
+	
+}
