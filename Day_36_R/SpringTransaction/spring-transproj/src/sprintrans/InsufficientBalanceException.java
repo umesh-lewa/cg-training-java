909e0d3ada3e832cba39fc9da245a133bd01d41e
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-05-14 09:48:56 +0530

    spring transaction

---
+++ Day_36_R/SpringTransaction/spring-transproj/src/sprintrans/InsufficientBalanceException.java
@@ -0,0 +1,16 @@
+package sprintrans;
+
+public class InsufficientBalanceException extends  Exception{
+	
+	String msg;
+	
+	public InsufficientBalanceException(String msg) {
+		this.msg=msg;
+	}
+	
+	@Override
+	public String toString() {
+		return this.msg;
+	}
+	
+}
