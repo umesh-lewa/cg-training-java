7504dd8aa2ae761c25fcd4ec15d63d1f0c7ba346
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-05-19 14:31:48 +0530

    coda4spring4

---
+++ Day_38/coda4springweb4/src/coda4springweb4/SpringConfig.java
@@ -0,0 +1,30 @@
+package coda4springweb4;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com")
+public class SpringConfig {
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
